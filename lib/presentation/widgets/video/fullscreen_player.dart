8e1c3d368b43e182f41fc1e7a725257049a327cb
oscar <oclemente.dscont@gmail.com>
2023-11-27 01:19:45 -0500

    nagregando librerias de vidoPLayer

---
+++ lib/presentation/widgets/video/fullscreen_player.dart
@@ -0,0 +1,58 @@
+
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class FullScreenPlayer extends StatefulWidget {
+
+  final String videoURL;
+  final String caption;
+
+  const FullScreenPlayer({
+      super.key,
+      required this.videoURL,
+      required this.caption
+  });
+
+  @override
+  State<FullScreenPlayer> createState() => _FullScreenPlayerState();
+}
+
+class _FullScreenPlayerState extends State<FullScreenPlayer> {
+
+  late VideoPlayerController controller;
+  
+  @override
+  void initState() {    
+    super.initState();
+    controller = VideoPlayerController.asset(widget.videoURL)
+    ..setVolume(0)
+    ..setLooping(true)
+    ..play();
+  }
+  @override
+  void dispose(){
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    return FutureBuilder(
+      future: controller.initialize(),
+      builder: (context,snapshot){
+        if(snapshot.connectionState!= ConnectionState.done)
+        {
+          return const Center(child: CircularProgressIndicator(strokeWidth: 2,));
+        }
+        
+        return AspectRatio(
+          aspectRatio: controller.value.aspectRatio,
+          child: VideoPlayer(controller),
+          );
+        
+      }
+    );
+  }
+}
